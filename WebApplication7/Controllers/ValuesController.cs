2b890a054d8e6f000805b9e2966f6a56160d739e
Ido Flatow <ido.flatow@gmail.com>
2023-03-23 11:20:44 +0000

    Add project files.

---
+++ WebApplication7/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Google.Apis.Auth.OAuth2;
+using log4net;
+
+namespace WebApplication7.Controllers
+{
+    public class LoggerController : ApiController
+    {
+        // GET api/logger
+        public string Get()
+        {
+            var credentials = GoogleCredential.GetApplicationDefault();
+
+            ILog log = LogManager.GetLogger(typeof(LoggerController));
+            log.Info("An exciting log entry!");
+            //LogManager.Flush(5000);
+            return "Wrote a log record using SA " + (credentials.UnderlyingCredential as ServiceAccountCredential).Id;
+        }
+    }
+}
